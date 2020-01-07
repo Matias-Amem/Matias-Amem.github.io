2734bfc8024f19beaa5c8bc67377a122c563635f
unknown <todomundoodeiaochefe@yahoo.com>
2020-01-06 22:37:41 -0300

    primeiro commit

---
+++ precache-manifest.0fc4ab577ecc2bfb9fd940b13da670a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1b20b7fc2c93889c81fc96c158b013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54fdf5e657591d50a630",
+    "url": "/static/css/main.136b34f4.chunk.css"
+  },
+  {
+    "revision": "26d7fe814b23cd5a4776",
+    "url": "/static/js/2.c04ba422.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.c04ba422.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54fdf5e657591d50a630",
+    "url": "/static/js/main.be0ba2f4.chunk.js"
+  },
+  {
+    "revision": "7df52723d36a62aa5396",
+    "url": "/static/js/runtime-main.3127cda7.js"
+  }
+]);
